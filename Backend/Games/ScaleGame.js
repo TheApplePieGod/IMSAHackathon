b49935dc2fdfbf67090f4c55a504b7921ec31e77
TheApplePieGod <fishplaysgame@gmail.com>
2022-05-13 22:32:23 -0500

    Game message handlers

---
+++ Backend/Games/ScaleGame.js
@@ -0,0 +1,13 @@
+
+// Define message type enum for all messages for this game
+const MessageType = {
+    None: 0
+};
+Object.freeze(MessageType);
+
+exports.handleMessage = (messageType, data) => {
+    switch (messageType) {
+        default: break;
+        
+    }
+}
--- Backend/MessageHandler.js
+++ Backend/MessageHandler.js
@@ -1,10 +1,31 @@
 const ws = require('ws');
+const { handleMessage: scaleGame } = require("./Games/ScaleGame");
 
+// Define game type enum for each unique game
+const GameType = {
+    None: 0,
+    Scale: 1,
+};
+Object.freeze(GameType);
+
+// Message format { messageType: number, gameType: number, data: string }
 exports.handleMessage = (message) => {
     try {
         jsn = JSON.parse(message);
-        console.log(jsn)
+        const messageType = message.messageType;
+        const gameType = message.gameType;
+        const data = message.data;
+
+        // Handoff the message to the related game type
+        switch (gameType) {
+            default: {
+                console.error("Unknown game type");
+            } break;
+            case GameType.Scale: {
+                scaleGame(messageType, data);
+            } break;
+        }
     } catch {
-        console.log("Invalid message");
+        console.error("Invalid message format");
     }
 }
